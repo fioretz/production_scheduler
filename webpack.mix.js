dfe509b4462e795072be2f6052daf5d701a047a3
fioretz <luca.fioretti94@gmail.com>
2021-04-20 13:25:59 +0200

    Modificato webpack.mix.js, composer.json, package.json, stili e script

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,4 +13,15 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
-    .sourceMaps();
+    .sourceMaps()
+    .scripts([
+        'node_modules/underscore/underscore.js',
+        'node_modules/jquery/dist/jquery.js',
+        'node_modules/bootstrap/dist/js/bootstrap.js',
+        'node_modules/backbone/backbone.js',
+    ], 'public/js/scripts.js')
+    .styles([
+        'node_modules/bootstrap/dist/css/bootstrap.css',
+        'node_modules/bootstrap/dist/css/bootstrap-grid.css',
+        'node_modules/bootstrap/dist/css/bootstrap-reboot.css'
+    ], 'public/css/bootstrap.css')
