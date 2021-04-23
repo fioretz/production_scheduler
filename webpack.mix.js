e5d6302306cbfdc986a23c97d8998e1a03dbdb1d
fioretz <luca.fioretti94@gmail.com>
2021-04-23 16:22:30 +0200

    Aggiunte libreria per gestione datatables

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,14 +14,18 @@ const mix = require('laravel-mix');
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
     .sourceMaps()
+    .copy('node_modules/datatables.net-dt/images', 'public/images')
     .scripts([
         'node_modules/underscore/underscore.js',
         'node_modules/jquery/dist/jquery.js',
         'node_modules/bootstrap/dist/js/bootstrap.js',
         'node_modules/backbone/backbone.js',
+        'node_modules/datatables.net/js/jquery.dataTables.js',
+        'node_modules/datatables.net-bs4/js/dataTables.bootstrap4.js'
     ], 'public/js/scripts.js')
     .styles([
         'node_modules/bootstrap/dist/css/bootstrap.css',
         'node_modules/bootstrap/dist/css/bootstrap-grid.css',
-        'node_modules/bootstrap/dist/css/bootstrap-reboot.css'
-    ], 'public/css/bootstrap.css')
+        'node_modules/bootstrap/dist/css/bootstrap-reboot.css',
+        'node_modules/datatables.net-bs4/css/dataTables.bootstrap4.css'
+    ], 'public/css/bootstrap.css');
