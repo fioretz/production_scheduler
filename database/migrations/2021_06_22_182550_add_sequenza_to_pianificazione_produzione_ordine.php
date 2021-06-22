1ebe5f809686b31d75b8b538964ccf3a7155d188
fioretz <luca.fioretti94@gmail.com>
2021-06-22 20:29:23 +0200

    Aggiunto campo sequenza tabella pianificazione_produzione_ordine

---
+++ database/migrations/2021_06_22_182550_add_sequenza_to_pianificazione_produzione_ordine.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddSequenzaToPianificazioneProduzioneOrdine extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('pianificazione_produzione_ordine', function (Blueprint $table) {
+            $table->integer('sequenza');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('pianificazione_produzione_ordine', function (Blueprint $table) {
+            $table->dropColumn('sequenza');
+        });
+    }
+}
