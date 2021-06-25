398a9e0c7c9df66ea1e1b4f4f5049c070938df87
fioretz <luca.fioretti94@gmail.com>
2021-06-25 13:03:49 +0200

    PianificazioneProduzione UI - aggiunto badge scadenza corretta

--- resources/views/pianificazioneproduzione/visualizzapianificazioneproduzione.blade.php
+++ resources/views/pianificazioneproduzione/visualizzapianificazioneproduzione.blade.php
@@ -26,7 +26,11 @@
                 @foreach($macchina['ordiniproduzione'] as $ordine)
                     <div class="card col-4" style="width: 18rem; @if ($ordine['erroreScadenza']) background-color: #f9d6d5; @else background-color: #d7f3e3; @endif">
                         <div class="card-body">
-                            @if ($ordine['erroreScadenza']) <span title="Scadenza non rispettata"><i class="fa fa-exclamation-triangle badge-errore-scadenza" aria-hidden="true"></i></span> @endif
+                            @if ($ordine['erroreScadenza'])
+                                <span title="Scadenza non rispettata"><i class="fa fa-exclamation-triangle badge-scadenza-errore" aria-hidden="true"></i></span>
+                            @else
+                                <span title="Scadenza rispettata"><i class="fa fa-check-circle badge-scadenza-ok" aria-hidden="true"></i></span>
+                            @endif
                             <h5 class="card-title">Numero Ordine: {{ $ordine['numeroordine'] }}</h5>
                             <h6 class="card-subtitle mb-2 text-muted">{{ $ordine['prodotto_codice'] }} - {{ $ordine['prodotto_descrizione'] }}</h6>
                             <br>
